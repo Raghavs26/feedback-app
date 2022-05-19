fe031c0734b6f3687aea048f546d0385a356fca9
Raghav <raghavsekhri2k1@gmail.com>
2022-05-19 10:27:21 +0530

    header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import FeedbackItem from "./components/FeedbackItem";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackItem />
+      </div>
+    </>
+  );
+};
+
+export default App;
